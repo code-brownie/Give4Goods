8641c76876c572577d0d584c2700c2ad9625e333
Aman Kumar <119786228+code-brownie@users.noreply.github.com>
2024-03-02 16:23:04 +0530

    Update app.py

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -1,38 +1,34 @@
-import argparse
-import io
-from PIL import Image
-
-import torch
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from PIL import Image
+import io
+import torch
 
 app = Flask(__name__)
 CORS(app)
 
+model = None  # Initialize model as None
 
-@app.route("/process_image", methods=["POST"])
+def load_model():
+    global model
+    if model is None:
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        model.eval()
+
+@app.route('/process_image', methods=['POST'])
 def process_image():
-    if "image" not in request.files:
+    load_model()  # Load the model before processing the image
 
-        return jsonify({"error": "No image file provided"})
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file provided'})
 
-    image_file = request.files["image"]
+    image_file = request.files['image']
     img_bytes = image_file.read()
     img = Image.open(io.BytesIO(img_bytes))
+
     results = model([img])
     data = results.pandas().xyxy[0].to_json(orient="records")
     return data
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
-    parser.add_argument("--port", default=5000, type=int, help="port number")
-    args = parser.parse_args()
-
-    try:
-        model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True)
-        model.eval()
-        print("Model loaded successfully.")
-    except Exception as e:
-        print("Error loading the model:", e)
-    app.run(host="0.0.0.0", port=args.port)
+    app.run(host="0.0.0.0", port=8000)
