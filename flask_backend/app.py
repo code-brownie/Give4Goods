e7b77f934aade87fa65982e69b25a97efd779653
Aman <jhonman02@gmail.com>
2024-03-07 23:32:13 +0530

    Made some changes regarding the python model

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -4,34 +4,59 @@ import time
 from PIL import Image
 
 import torch
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/process_image', methods=['POST'])
+torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
+model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True)
+model.eval()
+
+
+@app.route("/process_image", methods=["POST", "OPTIONS"])
 def process_image():
-    if 'image' not in request.files:
-        
-        return jsonify({'error': 'No image file provided'})
+    if request.method == "OPTIONS":
+        # Respond to OPTIONS preflight request
+        response = jsonify({"status": "ok"})
+        response.headers.add(
+            "Access-Control-Allow-Origin", "https://give4-goods.vercel.app"
+        )
+        response.headers.add("Access-Control-Allow-Methods", "POST")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        return response, 200
 
-    image_file = request.files['image']
-    img_bytes = image_file.read()
-    img = Image.open(io.BytesIO(img_bytes))
-    start_time = time.time()
-    results = model([img])
-    end_time = time.time()
-    processing_time = end_time - start_time
-    data = results.pandas().xyxy[0].to_json(orient="records")
-    print(processing_time)
-    return data;
+    try:
+        if "image" not in request.files:
+            return jsonify({"error": "No image file provided"}), 400
+
+        image_file = request.files["image"]
+        img_bytes = image_file.read()
+        img = Image.open(io.BytesIO(img_bytes))
+
+        start_time = time.time()
+        results = model([img])
+        end_time = time.time()
+
+        processing_time = end_time - start_time
+        data = results.pandas().xyxy[0].to_dict(orient="records")
+        print(data)
+        print(processing_time)
+
+        response = jsonify(data)
+        response.headers.add(
+            "Access-Control-Allow-Origin", "https://give4-goods.vercel.app"
+        )
+        print(response)
+        return response, 200
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return jsonify({"error": "Internal server error"}), 500
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
-
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True) 
-    model.eval()
     app.run(host="0.0.0.0", port=args.port)
