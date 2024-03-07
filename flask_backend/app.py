309e39c11dc6f8b5c96d31586286fb73f51f9da4
Aman Kumar <119786228+code-brownie@users.noreply.github.com>
2024-03-07 17:19:25 +0530

    Update app.py

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -4,23 +4,35 @@ import time
 from PIL import Image
 
 import torch
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+# Function to load the YOLOv5 model
+def load_yolov5_model():
+    torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    model.eval()
+    return model
+
 @app.route('/process_image', methods=['POST'])
 def process_image():
     if 'image' not in request.files:
-        
         return jsonify({'error': 'No image file provided'})
+
+    # Load the model inside the route
+    model = load_yolov5_model()
 
     image_file = request.files['image']
     img_bytes = image_file.read()
     img = Image.open(io.BytesIO(img_bytes))
     start_time = time.time()
+
+    # Use the locally defined model
     results = model([img])
+
     end_time = time.time()
     processing_time = end_time - start_time
     data = results.pandas().xyxy[0].to_json(orient="records")
@@ -31,7 +43,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
-    torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True) 
-    model.eval()
     app.run(host="0.0.0.0", port=args.port)
